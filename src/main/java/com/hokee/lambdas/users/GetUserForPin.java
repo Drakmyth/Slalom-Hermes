94ac0c77e10dac409b189f49af6b9bd30560b634
Drakmyth <shaun.hamman@gmail.com>
2016-01-28 12:37:27 -0800

    GetUserForPin

--- src/main/java/com/hokee/lambdas/users/GetUserForPin.java
+++ src/main/java/com/hokee/lambdas/users/GetUserForPin.java
@@ -1,11 +1,15 @@
 package com.hokee.lambdas.users;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.hokee.hermes.services.DbService;
 import com.hokee.shared.models.User;
 import com.hokee.shared.requests.GetUserForPinRequest;
 import com.hokee.shared.results.GetUserResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class GetUserForPin {
 
@@ -13,6 +17,20 @@ public class GetUserForPin {
 
 	public static GetUserResult handleRequest(final GetUserForPinRequest request, final Context context) {
 
-		return GetUserResult.Success(new User("id", "name"));
+		final String user_pin = request.getPin();
+		final User retVal;
+
+		try {
+			String sql = String.format("SELECT * FROM Users WHERE pin=='%s'", user_pin);
+			ResultSet results = DbService.executeQuery(sql);
+			retVal = new User(results.getString("user_guid"), user_pin);
+		} catch (final SQLException e) {
+			final String errorMessage = "Failed accessing non-existent column.";
+
+			logger.error(errorMessage, e);
+			return GetUserResult.Failure(errorMessage);
+		}
+
+		return GetUserResult.Success(retVal);
 	}
 }
